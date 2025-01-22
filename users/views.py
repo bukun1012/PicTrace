19b3fde1de30dbc65bc6403e51cfcd3aa2b0dca9
bukun1012 <bukun0972167325@gmail.com>
2025-01-22 18:53:20 +0800

    會員系統測試

---
+++ users/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+# 登入
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")  # 登入後跳轉到首頁
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", {"form": form})
+
+
+# 註冊
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    return render(request, "users/register.html", {"form": form})
+
+
+# 登出
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("home")
