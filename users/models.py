b669071a1abda7583aeb054c3d88908f3bc9e981
bukun1012 <bukun0972167325@gmail.com>
2025-01-25 01:45:18 +0800

    fix: users's views.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+
+class SimpleUserCreationForm(forms.Form):
+    username = forms.CharField(max_length=150, required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput, min_length=8, required=True)
+    password2 = forms.CharField(widget=forms.PasswordInput, min_length=8, required=True)
